98716e7f41eec581678075ba43704219b54e744d
Matthew Perkins <mfperkins@gmail.com>
2016-10-17 12:33:25 +0100

    5) Simple get request with query string working for /get path

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
 
 class TTDBServer < Sinatra::Base
+
+  set :sessions, true
+  set :session_secret, 'super secret'
+
+
   get '/' do
     "Hello World #{params[:name]}".strip
+  end
+
+  get '/get' do
+    key = params[:key]
+    @object = { 'myDog' => 'Beagle' }
+    if @object.has_key?(key)
+      value = @object[key]
+      "#{value}"
+    end
+  end
+
+  get '/set' do |somekey, somevalue|
+    "somekey somevalue"
+    " #{params.inspect}"
   end
 
   # start the server if ruby file executed directly
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -16,4 +16,19 @@ describe 'TTDBServer' do
     expect(last_response).to be_ok
     expect(last_response.body).to eq('Hello World')
   end
+
+  it "stores a key-value pair at '/set'" do
+    get '/set?myDog=Beagle'
+    puts last_response
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('{Dog: Beagle}')
+  end
+
+  it "returns a value '/get'" do
+    get '/get?key=myDog'
+    puts last_response
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Beagle')
+  end
+
 end
