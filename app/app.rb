022eb0df33494fb7eb6935ddb72b4469dcd9400b
Matthew Perkins <mfperkins@gmail.com>
2016-10-17 14:54:14 +0100

    11) Hooking up model to routes

--- app/app.rb
+++ app/app.rb
@@ -1,29 +1,38 @@
 require 'sinatra/base'
+require_relative './models/pairs'
 
 class TTDBServer < Sinatra::Base
 
   set :sessions, true
   set :session_secret, 'super secret'
 
+  helpers do
+    def current_pairs
+      session[:current_pairs] || Pairs.new
+    end
+  end
 
   get '/' do
     "Hello World #{params[:name]}".strip
   end
 
   get '/get' do
+    @current_pairs = current_pairs
     key = params[:key]
-    @object = session[:object]
-    if @object.has_key?(key)
-      value = @object[key]
-      "#{value}"
+    if @current_pairs.first(key) != nil
+      "#{@current_pairs.first(key)}"
+    else
+      "Sorry, we don't have that key-value pair stored"
     end
   end
 
   get '/set*' do
+    @current_pairs = current_pairs
     temp_object = params.first
-    @object = {temp_object[0] => temp_object[1]}
-    session[:object] = @object
-    "#{@object}"
+    @current_pairs.create(temp_object[0],temp_object[1])
+    session[:current_pairs] = @current_pairs
+    puts @current_pairs.get_all_pairs
+    "#{@current_pairs.get_all_pairs}"
   end
 
   run! if app_file == $0
